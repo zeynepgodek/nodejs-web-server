6b8c1821974bb74db69ec75e895849a412a2b4fa
zeynepgodek <zey.godek@gmail.com>
2019-01-11 18:32:34 +0300

    Heroku PORT güncellendi..

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var middleware = require("./middleware");
 var app = express();
-var PORT = process.env.PORT ;
+var PORT = process.env.PORT || 3000;
 
 //app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
